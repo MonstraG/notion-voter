5fc9991bd0b56d9cd96a5c67e1d1db5dd993224b
ArsenyGarelyshev <monstrag@gmail.com>
2022-05-10 18:50:06 +0500

    deployment test

--- pages/api/vote.ts
+++ pages/api/vote.ts
@@ -6,29 +6,25 @@ export const resetVote = (): Promise<unknown> => {
 	return setVoteState(emptyVoteData);
 };
 
-export const getVoteState = (): Promise<VoteData> => {
-	return redisRead().then((data) => {
+export const getVoteState = (): Promise<VoteData> =>
+	redisRead().then((data) => {
 		if (data == null) {
 			return emptyVoteData;
 		}
 		return data as VoteData;
 	});
-};
 
-export const setVoteState = (data: VoteData): Promise<unknown> => {
-	return redisWrite(data);
-};
+export const setVoteState = (data: VoteData): Promise<unknown> => redisWrite(data);
 
-export const changeReady = (voter: Voter, ready: { checked: boolean }): Promise<unknown> => {
-	return getVoteState().then((voteData) => {
+export const changeReady = (voter: Voter, ready: { checked: boolean }): Promise<unknown> =>
+	getVoteState().then((voteData) => {
 		ensureVoterEntered(voter, voteData);
 		voteData.ready[voter.name] = ready.checked;
 		return setVoteState(voteData);
 	});
-};
 
-export const changeVote = (voter: Voter, vote: Vote): Promise<unknown> => {
-	return getVoteState().then((voteData) => {
+export const changeVote = (voter: Voter, vote: Vote): Promise<unknown> =>
+	getVoteState().then((voteData) => {
 		ensureVoterEntered(voter, voteData);
 		if (!voteData.votes[vote.name]) {
 			voteData.votes[vote.name] = {};
@@ -37,14 +33,12 @@ export const changeVote = (voter: Voter, vote: Vote): Promise<unknown> => {
 		voteData.votes[vote.name][voter.name] = vote.checked;
 		return setVoteState(voteData);
 	});
-};
 
-export const finishVote = (): Promise<unknown> => {
-	return getVoteState().then((voteData) => {
+export const finishVote = (): Promise<unknown> =>
+	getVoteState().then((voteData) => {
 		voteData.done = true;
 		return setVoteState(voteData);
 	});
-};
 
 const ensureVoterEntered = (voter: Voter, voteData: VoteData) => {
 	if (voteData.users.every((u) => u.name != voter.name)) {
