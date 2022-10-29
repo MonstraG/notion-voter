0a70c40eee2061123749eba619eef3137daaaf5d
ArsenyGarelyshev <monstrag@gmail.com>
2022-10-29 20:19:36 +0200

    Switch to inter font

--- components/styles/theme.tsx
+++ components/styles/theme.tsx
@@ -3,6 +3,10 @@ import { createTheme, type ThemeOptions } from "@mui/material/styles";
 const themeOptions: ThemeOptions = {
 	palette: {
 		mode: "dark"
+	},
+	typography: {
+		fontFamily:
+			"Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'"
 	}
 };
 
