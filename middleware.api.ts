ec72c36c1f965bb6b532282ca2c96fe77b0656cf
ArsenyGarelyshev <monstrag@gmail.com>
2022-11-07 21:59:45 +0100

    Missed one

--- middleware.api.ts
+++ middleware.api.ts
@@ -2,7 +2,7 @@ import { withAuth } from "next-auth/middleware";
 
 const adminWhitelist: string[] = JSON.parse(process.env.ADMIN_WHITELIST);
 
-const debugMode = !Boolean(process.env.VERCEL);
+const debugMode = !process.env.VERCEL;
 
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
