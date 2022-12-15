5fc19e5643ac1051532162d87292eaff33b16f8e
ArsenyGarelyshev <monstrag@gmail.com>
2022-12-15 18:37:35 +0100

    - Update deps - enable swcMinify - fix invalid json in tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type import('next').NextConfig */
 const nextConfig = {
+	swcMinify: true,
 	reactStrictMode: true,
+	optimizeFonts: false, // https://github.com/vercel/next.js/issues/24640#issuecomment-879296080
 	compiler: {
 		emotion: true
 	},
